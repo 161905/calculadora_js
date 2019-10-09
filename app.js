6080c2a9a091f676d6b4fdedeab0d326d69edbe8
Fito <rodolfo.jaramillo.14@sansano.usm.cl>
2019-10-09 18:28:05 -0300

    Cambios
    Mejoras al 10000000%

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 var calculadora = {
     sumar: function sumarNumeros(n1, n2) {
+        //Boca sho te amo
        return n1 + n2
     },
 
     restar: function restarNumeros(n1, n2) {
-       //
+        //Sho te sigo a todos lados de corazon
 	return n1 - n2
     },
     
     multiplicar: function multiplicarNumeros(n1, n2) {
+        //Ponga mas huevos
         return n1*n2
     },
 
     dividir: function dividirNumeros(n1, n2) {
-        return n1/nn2
+        //Porque a Boca lo queremos ver campeon
+        return n1/n2
     }
 
 
